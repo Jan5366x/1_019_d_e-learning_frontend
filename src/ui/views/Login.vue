f7d3027ac6245a08d930fec262b14a1d970537e3
Tobias <ts@wunschloesung.com>
2020-03-21 14:34:49 +0100

    styled login

--- src/ui/views/Login.vue
+++ src/ui/views/Login.vue
@@ -1,33 +1,35 @@
 <template>
   <div class="px-4 flex flex-col w-full h-screen justify-center items-center">
-    <div class="w-full mb-4">
-      <h1 class="text-center font-semibold text-2xl">Login</h1>
-    </div>
-    <div class="w-full md:w-4/12 ">
-      <form
-        class="form login-form"
-        @submit.prevent="login"
-      >
-        <TextField
-          v-model="email"
-          :label="$t('email')"
-          required
-          type="email"
-        />
-        <TextField
-          v-model="password"
-          :label="$t('password')"
-          required
-          type="password"
-        />
-        <div class="mb-4 text-sm">
-          <router-link to="/">{{ $t('forgotPassword') }}</router-link>
-        </div>
-        <Button theme="primary" class="w-full" type="submit">
-          {{ $t('login') }}
-        </Button>
-      </form>
-    </div>
+    <section class="shadow-xl p-16">
+      <div class="w-full mb-4">
+        <h1 class="text-center font-semibold text-2xl">Login</h1>
+      </div>
+      <div class="w-full">
+        <form
+          class="form login-form"
+          @submit.prevent="login"
+        >
+          <TextField
+            v-model="email"
+            :label="$t('email')"
+            required
+            type="email"
+          />
+          <TextField
+            v-model="password"
+            :label="$t('password')"
+            required
+            type="password"
+          />
+          <div class="mb-4 text-sm">
+            <router-link to="/">{{ $t('forgotPassword') }}</router-link>
+          </div>
+          <Button theme="primary" class="w-full" type="submit">
+            {{ $t('login') }}
+          </Button>
+        </form>
+      </div>
+    </section>
   </div>
 </template>
 
