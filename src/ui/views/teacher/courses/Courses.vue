52ee5cd60805f4c9e64dffd365f05e717e15d2f1
Phil Schaffarzyk <ps@gridventures.de>
2020-03-22 14:13:56 +0100

    fixes

--- src/ui/views/teacher/courses/Courses.vue
+++ src/ui/views/teacher/courses/Courses.vue
@@ -1,14 +1,9 @@
 <template>
   <section class="p-4">
-    <Headline1>
-      {{ $t('courses') }}
-    </Headline1>
-
     <router-view />
 
-    <div class="grid grid-cols-1 grid-cols-1 md:grid-cols-2 md:grid-cols-3">
-      <TeacherCourses :teacher="{}" class="col col-span-2">
-      </TeacherCourses>
+    <div class="grid grid-cols-1 grid-cols-1 md:grid-cols-2 md:grid-cols-3 gap-4">
+      <TeacherCourses :teacher="{}" class="col col-span-2" />
       <Card class="col col col-span-1">
         <Headline2>
           {{ $t('reminder') }}
@@ -19,13 +14,13 @@
 </template>
 
 <script>
-import Headline1 from '../../../components/_globals/Typography/Headline1.vue';
-import Headline2 from '../../../components/_globals/Typography/Headline2.vue';
-import Card from '../../../components/_globals/Card.vue';
 import TeacherCourses from '../../../components/course/TeacherCourses.vue';
 
 export default {
   name: 'Courses',
-  components: { TeacherCourses, Headline2, Headline1, Card },
+
+  components: {
+    TeacherCourses,
+  },
 };
 </script>
