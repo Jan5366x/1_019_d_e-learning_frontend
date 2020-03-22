5e0b58e5f4fdc106654ac5e2391de8b26caf52e6
Tobias <ts@wunschloesung.com>
2020-03-22 13:56:08 +0100

    i added markup for courses etc.

---
+++ src/ui/components/course/TeacherCourses.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+    <TeacherSubjectOverview v-for="subject of data().subjects" :teacher="teacher" :subject="subject">
+    </TeacherSubjectOverview>
+  </section>
+</template>
+
+<script>
+import TeacherSubjectOverview from '../teacher/TeacherSubjectOverview.vue';
+import { subjects } from '../../../models/course/subject/Subjects';
+
+export default {
+  name: 'TeacherCourses',
+  components: { TeacherSubjectOverview },
+  props: {
+    teacher: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => {
+    return {
+      subject: subjects,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/ui/views/teacher/courses/Courses.vue
+++ src/ui/views/teacher/courses/Courses.vue
@@ -4,25 +4,26 @@
       {{ $t('courses') }}
     </Headline1>
 
-    <div class="grid grid-cols-1 md:grid-cols-2">
-      <div>
-      </div>
-      <div>
+    <div class="grid grid-cols-1 grid-cols-1 md:grid-cols-2 md:grid-cols-3">
+      <TeacherCourses :teacher="{}" class="col col-span-2">
+      </TeacherCourses>
+      <Card class="col col col-span-1">
         <Headline2>
           {{ $t('reminder') }}
         </Headline2>
-      </div>
+      </Card>
     </div>
-
   </section>
 </template>
 
 <script>
 import Headline1 from '../../../components/_globals/Typography/Headline1.vue';
 import Headline2 from '../../../components/_globals/Typography/Headline2.vue';
+import Card from '../../../components/_globals/Card.vue';
+import TeacherCourses from '../../../components/course/TeacherCourses.vue';
 
 export default {
   name: 'Courses',
-  components: { Headline2, Headline1 },
+  components: { TeacherCourses, Headline2, Headline1, Card },
 };
 </script>
